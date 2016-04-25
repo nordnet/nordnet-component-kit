2866ce1240829cfdf970bef9d1444468cd378b9c
andbro <andreas.brostrom@nordnet.se>
2016-04-25 15:01:48 +0200

    Fix numeric string value warning

--- src/components/development/development.jsx
+++ src/components/development/development.jsx
@@ -15,11 +15,11 @@ const styleSROnly = {
   position: 'absolute',
   width: '1px',
   height: '1px',
-  padding: '0',
+  padding: 0,
   margin: '-1px',
   overflow: 'hidden',
   clip: 'rect(0,0,0,0)',
-  border: '0',
+  border: 0,
 };
 
 function renderArrow(direction) {
