23ea8d274140f8499c6945b7e2aed474c50fcfc9
Andreas Broström <andreas.brostrom@gmail.com>
2016-05-11 17:24:02 +0200

    Ugly fix to make documentation work in safari

--- styleguide/renderer.jsx
+++ styleguide/renderer.jsx
@@ -2,31 +2,58 @@ import React, { PropTypes } from 'react';
 import classnames from 'classnames';
 import { Logo, Icon, variables } from 'nordnet-ui-kit';
 import './renderer.scss';
+import './intl.js';
 
-export default function Renderer({ title, components, toc, sidebar }) {
-  return (
-    <div className={ classnames('ReactStyleguidist-Layout__root', { 'ReactStyleguidist-Layout__sidebar': !sidebar }) }>
-      <main className="ReactStyleguidist-Layout__wrapper">
-        <div className="ReactStyleguidist-Layout__content">
-          <div className="ReactStyleguidist-Layout__components">
-            { components }
+class Renderer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: !window.Intl,
+    };
+  }
+
+  componentDidMount() {
+    if (this.state.isLoading) {
+      const that = this;
+      this.looper = setInterval(function loadIntl() {
+        if (window.Intl) {
+          clearInterval(this.looper);
+          that.setState({ isLoading: false });
+        }
+      }, 500);
+    }
+  }
+
+  render() {
+    const { title, components, toc, sidebar } = this.props;
+    if (this.state.isLoading) {
+      return (<span>Polyfilling your intl needs...</span>);
+    }
+
+    return (
+      <div className={ classnames('ReactStyleguidist-Layout__root', { 'ReactStyleguidist-Layout__sidebar': !sidebar }) }>
+        <main className="ReactStyleguidist-Layout__wrapper">
+          <div className="ReactStyleguidist-Layout__content">
+            <div className="ReactStyleguidist-Layout__components">
+              { components }
+            </div>
           </div>
-        </div>
-        <div className="ReactStyleguidist-Layout__sidebar">
-          <a href="https://github.com/nordnet/nordnet-component-kit">
-            <h1 className="ReactStyleguidist-Layout__heading">
-              <Logo />
-              { title.replace('Nordnet ', '').toLowerCase() }
-            </h1>
-          </a>
-          { toc }
-          <footer className="ReactStyleguidist-Layout__footer">
-            Made with <Icon type="heart" stroke={ variables.colorPrimary } />
-          </footer>
-        </div>
-      </main>
-    </div>
-  );
+          <div className="ReactStyleguidist-Layout__sidebar">
+            <a href="https://github.com/nordnet/nordnet-component-kit">
+              <h1 className="ReactStyleguidist-Layout__heading">
+                <Logo />
+                { title.replace('Nordnet ', '').toLowerCase() }
+              </h1>
+            </a>
+            { toc }
+            <footer className="ReactStyleguidist-Layout__footer">
+              Made with <Icon type="heart" stroke={ variables.colorPrimary } />
+            </footer>
+          </div>
+        </main>
+      </div>
+    );
+  }
 }
 
 Renderer.propTypes = {
@@ -35,3 +62,5 @@ Renderer.propTypes = {
   toc: PropTypes.node.isRequired,
   sidebar: PropTypes.bool,
 };
+
+export default Renderer;
