0c224acd32ee78cafe591e7c2c1764941aaf218a
Jesper Annebäck <jesper.anneback@gmail.com>
2016-11-11 15:24:03 +0100

    Added hover texts to Number component. (#356)
    * Added hover texts to Number component.
    
    * Title attribute is now set first in order to get overriding functionality. Made unit test more reliable.

--- test/components/number.test.js
+++ test/components/number.test.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import { FormattedNumber } from 'react-intl';
+import { IntlProvider } from 'react-intl';
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
 
-import Number from '../../src/components/number/number';
+import Number, { getFractionDigits } from '../../src/components/number/number';
 import Addon from '../../src/components/addon/addon';
+
+const intlProvider = new IntlProvider({ locale: 'en' }, {});
+const { intl } = intlProvider.getChildContext();
 
 describe('<Number />', () => {
   describe('with default parameter set (value)', () => {
@@ -13,18 +16,27 @@ describe('<Number />', () => {
 
     beforeEach(() => {
       component = shallow(
-        <Number
+        <Number.WrappedComponent
           value={defaultValue}
+          valueClass="test"
+          intl={intl}
         />);
     });
 
-    it('should default to set minimumFractionDigits and maximumFractionDigits to 2', () => {
-      expect(component.find(FormattedNumber).prop('maximumFractionDigits')).to.equal(2);
-      expect(component.find(FormattedNumber).prop('minimumFractionDigits')).to.equal(2);
+    it('minimumFractionDigits should be 2 and maximumFractionDigits to 2', () => {
+      expect(getFractionDigits(null, null, 2)).to.equal(2);
     });
 
-    it('should pass value through to FormattedNumber', () => {
-      expect(component.find(FormattedNumber).prop('value')).to.equal(defaultValue);
+    it('should consist of two spans', () => {
+      expect(component.find('span')).to.have.length(2);
+    });
+
+    it('should have value placed in the inner span', () => {
+      expect(component.find('.test').text()).to.equal('2.54');
+    });
+
+    it('should have value as title', () => {
+      expect(component.find('.number').prop('title')).to.equal('2.54');
     });
   });
 
@@ -58,48 +70,66 @@ describe('<Number />', () => {
 
     it('should default to valueDecimals when no ticks are supplied', () => {
       const decimals = 3;
-      const component = shallow(<Number value={1.4444} valueDecimals={decimals} />);
-      expect(component.find('FormattedNumber').prop('minimumFractionDigits')).to.equal(decimals);
+      const component = shallow(<Number.WrappedComponent intl={intl} valueClass="test" value={1.4444} valueDecimals={decimals} />);
+      expect(component.find('.test').text()).to.equal('1.444');
     });
 
     it('should display 4 decimals when value < 0.5', () => {
-      const component = shallow(<Number value={0.2} ticks={ticks} />);
-      expect(component.find('FormattedNumber').prop('minimumFractionDigits')).to.equal(4);
+      const component = shallow(<Number.WrappedComponent intl={intl} valueClass="test" value={0.2} ticks={ticks} />);
+      expect(component.find('.test').text()).to.equal('0.2000');
     });
 
     it('should display 3 decimals when value >= 1 and value < 2', () => {
-      const component = shallow(<Number value={1.22} ticks={ticks} />);
-      expect(component.find('FormattedNumber').prop('minimumFractionDigits')).to.equal(3);
+      const component = shallow(<Number.WrappedComponent intl={intl} valueClass="test" value={1.22} ticks={ticks} />);
+      expect(component.find('.test').text()).to.equal('1.220');
     });
 
     it('should display 1 decimals when value >= 500 and value < 1000', () => {
-      const component = shallow(<Number value={888} ticks={ticks} />);
-      expect(component.find('FormattedNumber').prop('minimumFractionDigits')).to.equal(1);
+      const component = shallow(<Number.WrappedComponent intl={intl} valueClass="test" value={888} ticks={ticks} />);
+      expect(component.find('.test').text()).to.equal('888.0');
     });
 
     it('should default to valueDecimals when there is no matching tick', () => {
       const decimals = 1;
-      const component = shallow(<Number value={444} ticks={ticks} valueDecimals={decimals} />);
-      expect(component.find('FormattedNumber').prop('minimumFractionDigits')).to.equal(decimals);
+      const component = shallow(<Number.WrappedComponent
+        intl={intl}
+        valueClass="test"
+        value={444}
+        ticks={ticks}
+        valueDecimals={decimals}
+      />);
+      expect(component.find('.test').text()).to.equal('444.0');
     });
 
     it('should find correct tick when value > to_price and value < to_price + tick', () => {
-      const component = shallow(<Number value={999.7} ticks={ticks} />);
-      expect(component.find('FormattedNumber').prop('minimumFractionDigits')).to.equal(1);
+      const component = shallow(<Number.WrappedComponent intl={intl} valueClass="test" value={999.7} ticks={ticks} />);
+      expect(component.find('.test').text()).to.equal('999.7');
     });
 
     it('should respect maximum number of decimals when no ticks given', () => {
       const valueMaxDecimals = 3;
-      const valueDecimals = 4;
-      const component = shallow(<Number value={99.12345} valueDecimals={valueDecimals} valueMaxDecimals={valueMaxDecimals} />);
-      expect(component.find('FormattedNumber').prop('maximumFractionDigits')).to.equal(valueMaxDecimals);
+      const valueDecimals = 2;
+      const component = shallow(<Number.WrappedComponent
+        intl={intl}
+        valueClass="test"
+        value={99.12345}
+        valueDecimals={valueDecimals}
+        valueMaxDecimals={valueMaxDecimals}
+      />);
+      expect(component.find('.test').text()).to.equal('99.123');
     });
 
     it('should respect minimum number of decimals when no ticks given', () => {
       const valueMinDecimals = 4;
-      const valueDecimals = 3;
-      const component = shallow(<Number value={99.12345} valueDecimals={valueDecimals} valueMinDecimals={valueMinDecimals} />);
-      expect(component.find('FormattedNumber').prop('minimumFractionDigits')).to.equal(valueMinDecimals);
+      const valueDecimals = 5;
+      const component = shallow(<Number.WrappedComponent
+        intl={intl}
+        valueClass="test"
+        value={99.1}
+        valueDecimals={valueDecimals}
+        valueMinDecimals={valueMinDecimals}
+      />);
+      expect(component.find('.test').text()).to.equal('99.1000');
     });
 
     it('should respect ticks when max and min num of decimals given', () => {
@@ -107,108 +137,114 @@ describe('<Number />', () => {
       const valueMaxDecimals = tick.decimals + 1;
       const valueMinDecimals = tick.decimals - 1;
       const value = tick.from_price + tick.tick;
-      const component = shallow(<Number
+      const component = shallow(<Number.WrappedComponent
+        intl={intl}
+        valueClass="test"
         value={value}
         valueMinDecimals={valueMinDecimals}
         valueMaxDecimals={valueMaxDecimals}
         ticks={[tick]}
       />);
-      expect(component.find('FormattedNumber').prop('maximumFractionDigits')).to.equal(tick.decimals);
-      expect(component.find('FormattedNumber').prop('minimumFractionDigits')).to.equal(tick.decimals);
+      expect(component.find('.test').text()).to.equal('0.0001');
     });
 
     it('should default to decimals when no ticks nor max num decimals', () => {
       const decimals = 3;
       const value = 3.1415;
-      const component = shallow(<Number value={value} ticks={[]} valueDecimals={decimals} />);
-      expect(component.find('FormattedNumber').prop('maximumFractionDigits')).to.equal(decimals);
-      expect(component.find('FormattedNumber').prop('minimumFractionDigits')).to.equal(decimals);
+      const component = shallow(<Number.WrappedComponent
+        intl={intl}
+        valueClass="test"
+        value={value}
+        ticks={[]}
+        valueDecimals={decimals}
+      />);
+      expect(component.find('.test').text()).to.equal('3.142');
     });
   });
 
   it('should add white-space: "nowrap" to Number', () => {
-    const component = shallow(<Number value={1} />);
+    const component = shallow(<Number.WrappedComponent intl={intl} value={1} />);
     expect(component.prop('style').whiteSpace).to.equal('nowrap');
   });
 
   it('should add class "number" to Number', () => {
-    const component = shallow(<Number value={1} />);
+    const component = shallow(<Number.WrappedComponent intl={intl} value={1} />);
     expect(component.hasClass('number')).to.equal(true);
   });
 
   it('should be possible to add a prefix', () => {
     const prefix = '123456789';
-    const component = shallow(<Number value={1} prefix={prefix} />);
+    const component = shallow(<Number.WrappedComponent intl={intl} value={1} prefix={prefix} />);
     expect(component.find(Addon).children().text()).to.equal(prefix);
   });
 
   it('should be possible to add a suffix', () => {
     const suffix = '987654321';
-    const component = shallow(<Number value={1} suffix={suffix} />);
+    const component = shallow(<Number.WrappedComponent intl={intl} value={1} suffix={suffix} />);
     expect(component.find(Addon).children().text()).to.equal(suffix);
   });
 
   it('should be possible to add a prefix and a suffix', () => {
     const data = ['123456789', '987654321'];
-    const component = shallow(<Number value={1} prefix={data[0]} suffix={data[1]} />);
+    const component = shallow(<Number.WrappedComponent intl={intl} value={1} prefix={data[0]} suffix={data[1]} />);
     component.find(Addon).forEach((node, index) => {
       expect(node.children().text(), data[index]);
     });
   });
 
   it('should add default class addon--left to prefix', () => {
-    const component = shallow(<Number value={1} prefix="." />);
+    const component = shallow(<Number.WrappedComponent intl={intl} value={1} prefix="." />);
     expect(component.find(Addon).hasClass('addon--left')).to.equal(true);
   });
 
   it('should add default class addon--right to suffix', () => {
-    const component = shallow(<Number value={1} suffix="." />);
+    const component = shallow(<Number.WrappedComponent intl={intl} value={1} suffix="." />);
     expect(component.find(Addon).hasClass('addon--right')).to.equal(true);
   });
 
   it('should be able to add custom classes to prefix', () => {
     const prefixClass = 'custom-class';
-    const component = shallow(<Number value={1} prefix="." prefixClass={prefixClass} />);
+    const component = shallow(<Number.WrappedComponent intl={intl} value={1} prefix="." prefixClass={prefixClass} />);
     expect(component.find(Addon).hasClass(prefixClass)).to.equal(true);
   });
 
   it('should be able to add custom classes to suffix', () => {
     const suffixClass = 'custom-class';
-    const component = shallow(<Number value={1} suffix="." suffixClass={suffixClass} />);
+    const component = shallow(<Number.WrappedComponent intl={intl} value={1} suffix="." suffixClass={suffixClass} />);
     expect(component.find(Addon).hasClass(suffixClass)).to.equal(true);
   });
 
   it('should be able to add custom classes to value', () => {
     const valueClass = 'custom-class';
-    const component = shallow(<Number value={1} valueClass={valueClass} />);
-    expect(component.find(FormattedNumber).parent().hasClass(valueClass)).to.equal(true);
+    const component = shallow(<Number.WrappedComponent intl={intl} value={1} valueClass={valueClass} />);
+    expect(component.find('.custom-class')).to.have.length(1);
   });
 
   it('should be able to add custom styles to prefix', () => {
     const prefixStyle = { color: 'custom' };
-    const component = shallow(<Number value={1} prefix="." prefixStyle={prefixStyle} />);
+    const component = shallow(<Number.WrappedComponent intl={intl} value={1} prefix="." prefixStyle={prefixStyle} />);
     expect(component.find(Addon).prop('style')).to.deep.equal(prefixStyle);
   });
 
   it('should be able to add custom styles to suffix', () => {
     const suffixStyle = { color: 'custom' };
-    const component = shallow(<Number value={1} suffix="." suffixStyle={suffixStyle} />);
+    const component = shallow(<Number.WrappedComponent intl={intl} value={1} suffix="." suffixStyle={suffixStyle} />);
     expect(component.find(Addon).prop('style')).to.deep.equal(suffixStyle);
   });
 
   it('should be able to add custom styles to value', () => {
     const valueStyle = { color: 'custom' };
-    const component = shallow(<Number value={1} valueStyle={valueStyle} />);
-    expect(component.find(FormattedNumber).parent().prop('style')).to.deep.equal(valueStyle);
+    const component = shallow(<Number.WrappedComponent intl={intl} value={1} valueStyle={valueStyle} />);
+    expect(component.find('.number > span').prop('style')).to.equal(valueStyle);
   });
 
   it('should be able to add custom prefixSeparator', () => {
-    const component = shallow(<Number value={1} prefix=":" prefixSeparator=")" />);
+    const component = shallow(<Number.WrappedComponent intl={intl} value={1} prefix=":" prefixSeparator=")" />);
     expect(component.find(Addon).children().text()).to.equal(':)');
   });
 
   it('should be able to add custom suffixSeparator', () => {
-    const component = shallow(<Number value={1} suffix="(" suffixSeparator=":" />);
+    const component = shallow(<Number.WrappedComponent intl={intl} value={1} suffix="(" suffixSeparator=":" />);
     expect(component.find(Addon).children().text()).to.deep.equal(':(');
   });
 });
