d47e763a3c273683f2fdf30ad256b3afc5aa50e1
Robin Engström <robineng91@gmail.com>
2017-01-26 10:23:37 +0100

    Add a on-click-outside HOC (#474)
    * Add a on-click-outside HOC component, make slight modifications to test env in order to support mount and correct event listeners
    
    * small changes to hoc docs
    
    * more small changes to hoc docs
    
    * even more small changes to hoc docs

---
+++ test/hocs/on-click-outside.test.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { expect } from 'chai';
+import { mount } from 'enzyme';
+import sinon from 'sinon';
+
+import OnClickOutsideDummyComponent from './on-click-outside-dummy-component';
+
+describe('onClickOutside HOC', () => {
+  let component;
+  let sandbox;
+  let clickedOutsideSpy;
+  let addEventSpy;
+  let removeEventSpy;
+
+  beforeEach(() => {
+    sandbox = sinon.sandbox.create();
+    clickedOutsideSpy = sandbox.spy();
+    addEventSpy = sandbox.spy(document, 'addEventListener');
+    removeEventSpy = sandbox.spy(document, 'removeEventListener');
+    component = mount(
+      <OnClickOutsideDummyComponent clickedOutsideSpy={clickedOutsideSpy} />,
+      { attachTo: document.getElementById('app') },
+    );
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  it('should subscribe to click event on mount', () => {
+    expect(addEventSpy).to.have.been.calledOnce();
+  });
+
+  it('should unsubscribe from click events on unmount', () => {
+    component.unmount();
+    expect(removeEventSpy).to.have.been.calledTwice();
+  });
+
+  it('should call handleClickOutside if click is made outside chosen element', () => {
+    document.getElementById('top').click();
+    expect(clickedOutsideSpy).to.have.been.calledOnce();
+  });
+
+  it('should not call handleClickOutside if click is made on chosen element', () => {
+    document.getElementById('mid').click();
+    expect(clickedOutsideSpy).to.not.have.been.called();
+  });
+
+  it('should not call handleClickOutside if click is made inside chosen element', () => {
+    document.getElementById('bot').click();
+    expect(clickedOutsideSpy).to.not.have.been.called();
+  });
+});
