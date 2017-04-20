a17b5328d6da901f3ea9b85bd28ebf57bd3182f2
johsod <johan.kitti@nordnet.se>
2017-04-20 11:04:44 +0200

    added some functionality to Development component.

--- src/components/development/development.jsx
+++ src/components/development/development.jsx
@@ -7,9 +7,9 @@ import PercentComponent from '../percent/percent';
 function renderSign(direction) {
   switch (direction) {
     case 'positive':
-      return (<span dangerouslySetInnerHTML={{ __html: '&plus; ' }} />);
+      return (<span dangerouslySetInnerHTML={{ __html: '&plus; ' }} />); // eslint-disable-line
     case 'negative':
-      return (<span dangerouslySetInnerHTML={{ __html: '&minus; ' }} />);
+      return (<span dangerouslySetInnerHTML={{ __html: '&minus; ' }} />); // eslint-disable-line
     default:
       return (<span />);
   }
@@ -35,6 +35,10 @@ export default function Development({
   className,
   maxDecimals,
   minDecimals,
+  positiveDirectionColor,
+  neutralDirectionColor,
+  negativeDirectionColor,
+  fontStyle,
   ...rest
 }) {
   const components = {
@@ -46,17 +50,31 @@ export default function Development({
   const Component = components[type] || components.number;
   const classes = classNames(`number--${developmentDirection}`, className);
 
+  const fontStyles = Object.assign({}, fontStyle);
+
+  if (developmentDirection === 'positive' && positiveDirectionColor) {
+    fontStyles.color = positiveDirectionColor;
+  }
+  if (developmentDirection === 'negative' && negativeDirectionColor) {
+    fontStyles.color = negativeDirectionColor;
+  }
+  if (developmentDirection === 'neutral' && neutralDirectionColor) {
+    fontStyles.color = neutralDirectionColor;
+  }
+
   return (
-    <Component
-      {...rest}
-      className={classes}
-      value={Math.abs(parseFloat(value))}
-      valueDecimals={decimals}
-      valueMaxDecimals={maxDecimals}
-      valueMinDecimals={minDecimals}
-      prefix={renderSign(developmentDirection)}
-      prefixStyle={rest.prefixStyle}
-    />
+    <span style={fontStyles}>
+      <Component
+        {...rest}
+        className={classes}
+        value={Math.abs(parseFloat(value))}
+        valueDecimals={decimals}
+        valueMaxDecimals={maxDecimals}
+        valueMinDecimals={minDecimals}
+        prefix={renderSign(developmentDirection)}
+        prefixStyle={rest.prefixStyle}
+      />
+    </span>
   );
 }
 
@@ -68,6 +86,10 @@ Development.propTypes = {
   direction: React.PropTypes.oneOf(['positive', 'negative', 'neutral']),
   maxDecimals: React.PropTypes.number,
   minDecimals: React.PropTypes.number,
+  positiveDirectionColor: React.PropTypes.string,
+  neutralDirectionColor: React.PropTypes.string,
+  negativeDirectionColor: React.PropTypes.string,
+  fontStyle: React.PropTypes.object,
 };
 
 Development.defaultProps = {
